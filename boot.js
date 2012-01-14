eaf1c21aa6108fb41669e559d9b2a5c421e6b5d6
Duke <duke@riseup.net>
2012-01-14 21:57:03 -0200

    added boot file

---
+++ boot.js
@@ -0,0 +1,5 @@
+Ti.include("/lib/appcelerator_on_rails/vendor/inheritance.js");
+Ti.include("/lib/appcelerator_on_rails/vendor/underscore.js");
+Ti.include("/lib/appcelerator_on_rails/core_ext/string.js");
+Ti.include("/lib/appcelerator_on_rails/view.js");
+Ti.include("/lib/appcelerator_on_rails/controller.js");
