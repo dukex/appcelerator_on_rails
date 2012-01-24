9ab97850501296d9239d20053b14f3db774c0e8e
Duke <duke@riseup.net>
2012-01-24 16:48:46 -0200

    string should load only in controllers

--- boot.js
+++ boot.js
@@ -1,5 +1,4 @@
 Ti.include("/lib/appcelerator_on_rails/vendor/inheritance.js");
 Ti.include("/lib/appcelerator_on_rails/vendor/underscore.js");
-Ti.include("/lib/appcelerator_on_rails/core_ext/string.js");
 Ti.include("/lib/appcelerator_on_rails/view.js");
 Ti.include("/lib/appcelerator_on_rails/controller.js");
--- controller.js
+++ controller.js
@@ -1,11 +1,13 @@
-Ti.App.Controller = Class.extend({
-    init: function(win, name)
-    {
-      this.win = win
-      win.controllerClass = this;
+Ti.include("/lib/appcelerator_on_rails/core_ext/string.js");
 
-      Ti.include("/views/" + name + ".js");
-      var class_view = (name + "_view").classify();
-      this.view = new Ti.App[class_view](win, this);
-    }
+Ti.App.Controller = Ti.App.Class.extend({
+  init: function(win, name)
+  {
+    this.win = win;
+    win.controllerClass = this;
+
+    Ti.include("/views/" + name + ".js");
+    var class_view = ("" + name + "_view").classify();
+    this.view = new Ti.App[class_view](win, this);
+  }
 });
