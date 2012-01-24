3c9b310b0c19adfed0501e1f522e694de4833482
Duke <duke@riseup.net>
2012-01-24 16:52:47 -0200

    using namespace classs

--- view.js
+++ view.js
@@ -1,9 +1,7 @@
-Ti.App.View = Class.extend({
-    init: function(win, controller)
-    {
-        this.win = win;
-        this.controller = controller;
-
-        win.viewClass = this;
-    }
+Ti.App.View = Ti.App.Class.extend({
+  init: function(win, controller)
+  {
+    this.win = win;
+    this.controller = controller;
+  }
 });
